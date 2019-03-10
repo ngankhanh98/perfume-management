29e41bd5779e24cca86aa62ff44536d43d04a36b
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2019-03-10 09:55:47 +0700

    Load data done.

--- perfume-management/MainWindow.xaml.cs
+++ perfume-management/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,19 @@ namespace perfume_management
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Load_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @"Data Source=.\SQLEXPRESS;Initial Catalog=Perfume;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(path);
+
+            string query = @"SELECT * FROM PERFUME";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            datagrid_Items.DataContext = table.DefaultView;
         }
     }
 }
